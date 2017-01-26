c80ed2e94a2704b7eb4af316e93d737c405a0b6c
Abdul Barek <barek2k2@gmail.com>
2017-01-26 14:19:31 +0600

    navigation br added

---
+++ platforms/android/assets/www/js/application.js
@@ -0,0 +1,19 @@
+$(function(){
+
+});
+
+function toggle_sidebar()
+{
+    var sidebar = document.getElementById("sidebar");
+
+    console.log(sidebar.style.left);
+
+    if(sidebar.style.left == "-200px")
+    {
+        sidebar.style.left = "0px";
+    }
+    else
+    {
+        sidebar.style.left = "-200px";
+    }
+}
